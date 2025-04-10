4ed852bf068433d08c8344106db31d6d6812773a
Rohit vihan k <167102294+Rohitvihan@users.noreply.github.com>
2025-04-10 22:33:36 +0530

    Add files via upload

---
+++ Longest_sub_array_sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void longest_sub(int n, int arr[], int k)
+{
+    int len = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for(int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if(sum == k)
+            {
+                len = max(len, j - i + 1);
+            }
+        }
+    }
+    cout << "Length of longest subarray with sum " << k << ": " << len << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 7, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 12;
+
+    longest_sub(n, arr, k);
+
+    return 0;
+}
