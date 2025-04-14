7d86f98d2299af19fd5a9f4d7bad1ab6e13faa08
Rohit vihan k <167102294+Rohitvihan@users.noreply.github.com>
2025-04-14 10:09:04 +0530

    Add files via upload

---
+++ second_Largest_element.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int largest_element(int arr[],int n)
+{
+    int maxi=arr[0];
+    int sec_larg=-1;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>maxi)
+        {
+            sec_larg=maxi;
+            maxi=arr[i];
+        }
+        if(arr[i]>sec_larg && arr[i]!=maxi)
+        {
+            sec_larg=arr[i];
+        }
+    }
+    return sec_larg;
+}
+int main()
+{
+    int n;
+    cout<<"enter the size:";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the array elements:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int res=largest_element(arr,n);
+    cout<<"the second largest element in the array is:"<<res;
+    return 0;
+}
